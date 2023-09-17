7da3a8fa76f64f11a88aa90d5599f2f6a99f5830
Tripadakalena612 <145289059+Tripadakalena612@users.noreply.github.com>
2023-09-18 00:41:59 +0530

    Add files via upload

---
+++ attendance/js/profile.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById("menu-toggle");
+const menuItems = document.getElementById("menu-items");
+
+menuToggle.addEventListener("click", () => {
+  menuItems.classList.toggle("active");
+});
